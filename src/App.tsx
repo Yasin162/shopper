8713fece622223fc787721ce68a893b780621fea
Yasin <yasinabdussaburr@gmail.com>
2022-03-19 19:42:20 -0400

    imported state, query, also some components, and styles to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-
+import { useState } from "react";
+import { useQuery } from "react-query";
+// components
+import Drawer from '@material-ui/core/Drawer';
+import { LinearProgress } from "@material-ui/core";
 
 const App = () => {
   return (
